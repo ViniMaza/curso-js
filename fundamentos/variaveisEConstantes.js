e48a7c5898ad5c77bab2ee1c5295b92ac5793b5d
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-08 21:05:37 -0300

    diferenca entre var, let e const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,23 @@
+/*  var
+    variavel global
+*/
+/*  let
+    só funciona num bloco de código
+*/
+var a = 3
+let b = 4
+
+var a = 9
+var a = 5
+
+console.log(a, b)
+
+/*  const
+    nao pode ser reatribuida
+    nada impede de ser alterada
+    só funciona num bloco de código
+*/ 
+
+const c = 30393
+
+console.log(c)
