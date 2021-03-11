fbb7f9fefd3fc3afe6dbd9d9ae56534094446ad8
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-10 22:16:16 -0300

    introducão a objeto e JSON

---
+++ fundamentos/objeto.js
@@ -0,0 +1,11 @@
+//Objeto == coleção de pares, chave e valor
+const prod1 = {}
+prod1.nome = 'Samsumg S7'
+prod1.preco = '3349.90'
+prod1['Desconto Legal'] = 0.40 //não usar atributos com espaço
+
+console.log(prod1)
+
+// JSON (formato textual)
+
+'{ "nome": Camisa Polo, "preco": 79.90 }'
