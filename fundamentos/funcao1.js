ab5ad7db393a70c4d6c2078db3379a8d20ba7d0e
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-11 18:54:41 -0300

    funcão com retorno e sem retorno

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,16 @@
+// Funcão sem retorno
+function imprimirSoma(a, b){
+    console.log(a + b)
+}
+imprimirSoma(3, 5)
+imprimirSoma(3)
+imprimirSoma(3, 5, 4, 48, 49)
+imprimirSoma()
+
+// Funcao com retorno
+
+function somar(a, b){
+    return a + b
+}
+
+console.log(somar(2, 9))
