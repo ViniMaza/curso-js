8b4deb0d48deab9a3f684ee99d4c17c12af87280
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-18 12:21:10 -0300

    operadores aritméticos

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [3, 4, 5, 6]
+
+const soma = a + b + c + d
+const subtracao = a - b
+const multiplicacao = a * c
+const divisao = d / b
+const resto = c % d
+
+console.log(soma, subtracao, multiplicacao, divisao, resto)
