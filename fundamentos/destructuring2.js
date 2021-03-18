d5b4e2dbf5236e5f4621f29b754aaf9c330397a6
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-18 10:20:12 -0300

    destructuring com array

---
+++ fundamentos/destructuring2.js
@@ -0,0 +1,8 @@
+const [a] = [10]
+console.log(a)
+
+const [n1, , n3, , n5, n6] = [2, 4, 5, 7]
+console.log(n1, n3, n5, n6)
+
+const [,[, , , nota]] = [[3, 4, 6, 6, 4, 3, 3], [3, 8, 0, 9, 3]]
+console.log(nota)
