18ea79dc89b22a008be326b7015ed97fe7e3dbfb
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-18 11:35:33 -0300

    destructuring numa funcão

---
+++ fundamentos/destructuring3.js
@@ -0,0 +1,9 @@
+function rand({ min = 0, max = 1000 }){
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+console.log(rand({}))
+console.log(rand({min: 500}))
+
+
