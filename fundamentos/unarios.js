0aa6135105a4f5ffb4a57ba87338b66a4ce02cbb
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-19 09:12:26 -0300

    operadores unários

---
+++ fundamentos/unarios.js
@@ -0,0 +1,11 @@
+let num1 = 1
+let num2 = 2
+
+num1++ // pós fixada
+console.log(num1)
+--num1 // pré fixada
+console.log(num1)
+
+console.log(++num1 === num2--) // só vai ser decrementado, depois de comparar
+console.log(num1 === num2)
+
