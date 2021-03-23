05b5937d127d7067bc99b78105af84cd8309dc46
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-23 18:12:10 -0300

    funcão anônima

---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,12 @@
+// Função arrow sempre é um função anônima
+let dobra = function(a){
+    return 2 * a
+}
+
+dobro = a => 2 * a // return implícito
+console.log(dobro(Math.PI))
+
+ola = () =>'Olá'
+ola = _ => 'Ola' // possui um param
+
+console.log(ola())
