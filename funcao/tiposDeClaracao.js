479486ffc24e847df8e5c23e9751f87c525156f9
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-28 16:55:57 -0300

    tipos de funcao

---
+++ funcao/tiposDeClaracao.js
@@ -0,0 +1,14 @@
+//function declaration
+function soma (a, b){
+    return a + b
+}
+
+// Function expression
+const sub = function (a, b){ 
+    return a - b
+}
+
+// named function expression
+const mult = function mult(a, b){
+    return a * b
+}
