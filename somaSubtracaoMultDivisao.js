025fabcf13a4a3f3df24220d26e576c7fab719ab
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-30 22:36:21 -0300

    1. calculadora básica

--- somaSubtracaoMultDivisao.js
+++ somaSubtracaoMultDivisao.js
@@ -3,9 +3,9 @@ function calculadoraBasica(numero1, numero2){
     subtracao = numero1 - numero2
     multiplica = numero1 * numero2
     divisao = numero1 / numero2
-    console.log(`O resultado da soma é ${soma}\n
-    O resultado da subtração é ${subtracao}\n
-    O resultado da multiplicação é ${multiplica}\n
-    O resultado da divisão é ${divisao}\n`)
+    console.log(`O resultado da soma é ${soma}
+    O resultado da subtração é ${subtracao}
+    O resultado da multiplicação é ${multiplica}
+    O resultado da divisão é ${divisao}`)
 }
 calculadoraBasica(10,2)
