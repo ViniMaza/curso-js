b908edc6b4f7bf37f780121f6a4a1aadbca853f9
Vinicius Marques <vinicius013.souza@gmail.com>
2021-03-31 09:59:11 -0300

    4. divisão e resto

---
+++ dividendoDivisor.js
@@ -0,0 +1,7 @@
+function divisaoResto(dividendo, divisor){
+    let result = dividendo / divisor
+    let resultResto = dividendo % divisor
+    console.log(`O resultado da divisão é ${result}
+O resto da divisão é ${resultResto}`)
+}
+divisaoResto(10,2)
