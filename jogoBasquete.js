57dc620647d63c2bb4998dc09554d86137d2615b
Vinicius Marques <vinicius013.souza@gmail.com>
2021-04-01 17:48:44 -0300

    8. avaliacão de pontuacão

---
+++ jogoBasquete.js
@@ -0,0 +1,21 @@
+let stringPontuacoes = "10, 20, 20, 8, 25, 3, 0, 30, 1"
+
+function avaliaPontuacao(stringPontuacoes){
+    let pontuacoes = stringPontuacoes.split(", ")
+    let qtdQuebraDeRecords = 0
+    let piorJogo = 1
+    let maiorPontuacao = pontuacoes[0]
+    let menorPontuacao = pontuacoes[0]
+
+    for(let i = 1; i < pontuacoes.length; i++){
+        if(pontuacoes[1] > menorPontuacao){
+            maiorPontuacao = pontuacoes[i]
+            qtdQuebraDeRecords++
+        }else if(pontuacoes[i] < menorPontuacao){
+            menorPontuacao = pontuacoes[i]
+            piorJogo = i+1
+        }
+    }
+    return [qtdQuebraDeRecords, piorJogo]
+}
+console.log(avaliaPontuacao(stringPontuacoes))
