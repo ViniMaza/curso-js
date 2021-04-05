153e4624a9b742cf142ad34f873ce34a0b4487e5
Vinicius Marques <vinicius013.souza@gmail.com>
2021-04-05 11:01:59 -0300

    5 formas de criar um objeto

--- objeto/criandoObjeto.js
+++ objeto/criandoObjeto.js
@@ -17,3 +17,27 @@ function Produto(nome, preco, desc){
 const p1 = new Produto ('Caneta', 7.99, 0.15)
 const p2 = new Produto ('Notbook', 2998.99, 0.25)
 console.log(p1.getPrecoComDescomto(), p2.getPrecoComDescomto())
+
+// Função Factory
+function criarFuncionario(nome, salarioBase, faltas){
+    return{
+        nome,
+        salarioBase,
+        faltas,
+        getSalario(){
+            return (salarioBase / 30) * (30 - faltas)
+        }
+    }
+}
+const f1 = criarFuncionario('Vinícius', 8000.00, 3)
+const f2 = criarFuncionario('Bruninha', 10000.00, 2)
+console.log(f1.getSalario().toFixed(2), f2.getSalario().toFixed(2))
+
+// Object.create
+const filha = Object.create(null)
+filha.nome = 'Ana'
+console.log(filha)
+
+// Uma função famosa que retorna um Objeto..
+const fromJSON = JSON.parse('{"info":  "Sou um JSON"}')
+console.log(fromJSON.info)
