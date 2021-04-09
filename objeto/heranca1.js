0cf722d5c1de1e5c030ecbae85ea3f7674357307
Vinicius Marques <vinicius013.souza@gmail.com>
2021-04-09 20:26:52 -0300

    Object heraca

--- objeto/heranca1.js
+++ objeto/heranca1.js
@@ -10,3 +10,9 @@ const volvo = {
 
 console.log(ferrari.__proto__)
 console.log(ferrari.__proto__ === Object.prototype)
+console.log(volvo.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
+
+function MeuObjetivo(){}
+console.log(typeof Object, typeof MeuObjetivo)
+console.log(Object.prototype, MeuObjetivo.prototype)
