670f04c6f0b021bef81e256b0f30ab3beb750fcb
Vinicius Marques <vinicius013.souza@gmail.com>
2021-04-11 02:41:57 -0300

    mais sobre prototype

---
+++ objeto/heranca4.js
@@ -0,0 +1,23 @@
+function MeuObjeto() {}
+console.log(MeuObjeto.prototype) // é um objeto
+
+const obj1 = new MeuObjeto
+const obj2 = new MeuObjeto
+console.log(obj1.__proto__ === obj2.__proto__)
+console.log(MeuObjeto.prototype === obj1.__proto__)
+
+MeuObjeto.prototype.nome = 'Anônimo'
+MeuObjeto.prototype.falar = function(){
+    console.log(`Bom dia! Meu nome é ${this.nome}!`)
+}
+
+obj1.falar() // utilizou o nome do protótipo
+
+obj2.nome = 'Vinícius'
+obj2.falar()
+
+// Resumindo a Loucura..
+console.log((new MeuObjeto).__proto__ === MeuObjeto.prototype)
+console.log(MeuObjeto.__proto__ === Function.prototype)
+console.log(Function.prototype.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
